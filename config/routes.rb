ed3e3d67dd3ddaef96099dd6a08bd7dcd1eb8b73
Shama Anjum <shamaanjum09@outlook.com>
2018-08-03 10:20:43 -0700

    CRUD operations on posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root of my site
-  root 'posts#index'
+  root "posts#index", as: "home"
   # route for about - controller#action
-  get 'about' => 'pages#about'
+  get "about" => "pages#about", as: "about"
   #Example resource route(maps HTTP verbs to controller actions)
   resources :posts
-  
 end
